2bcb6f23b5002bb7d7e3101aa1d024e33650bb95
YossefMohamed <yossefmohamed112233@gmail.com>
2023-01-09 22:29:48 +0200

    fixing profileImage issue

--- src/routes/api/user/imageUpload.ts
+++ src/routes/api/user/imageUpload.ts
@@ -11,13 +11,25 @@ router.post(
     console.log(req.body);
     const user =
       req.body.type === "profile"
-        ? await User.findByIdAndUpdate(req.session.user, {
-            profilePic: req.file.filename,
-          })
-        : await User.findByIdAndUpdate(req.session.user, {
-            coverPic: req.file.filename,
-          });
-
+        ? await User.findByIdAndUpdate(
+            req.session.user,
+            {
+              profilePic: req.file.filename,
+            },
+            {
+              new: true,
+            }
+          )
+        : await User.findByIdAndUpdate(
+            req.session.user,
+            {
+              coverPic: req.file.filename,
+            },
+            {
+              new: true,
+            }
+          );
+    req.session.user = user;
     res.status(200).json({
       status: "ok",
       data: user,
