e7ca7d677653e346b8402549d7a065044f09085e
YossefMohamed <yossefmohamed112233@gmail.com>
2023-01-16 09:49:54 +0200

    add user to readBy in the message

--- src/routes/api/message/getMessagesByChat.ts
+++ src/routes/api/message/getMessagesByChat.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { ObjectId } from "mongodb";
 import Message from "../../../schemas/messageSchema";
 
 const router = Router();
@@ -7,7 +8,14 @@ router.get("/:id", async (req: any, res, next) => {
   const messages = await Message.find({
     chat: req.params.id,
   }).populate("sender");
-
+  await Message.updateMany(
+    {
+      chat: req.params.id,
+    },
+    {
+      $addToSet: { readBy: req.session.user._id },
+    }
+  );
   res.status(200).json({
     status: "ok",
     data: messages,
