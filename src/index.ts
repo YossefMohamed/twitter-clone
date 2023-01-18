17471943b74086896f75234a0071f50eccbb2167
Yossef Mohamed <47724276+YossefMohamed@users.noreply.github.com>
2023-01-18 21:19:39 +0200

    fixing port issue

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { app } from "./app";
 
-const port = process.env.PORT! || 3000;
+const port =  3000;
 const server = app.listen(port, () => {
   console.log(`Express is listening at http://localhost:${port}`);
 });
